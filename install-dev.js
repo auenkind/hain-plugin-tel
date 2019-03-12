a81a5a2ce2a965ecb6f5ebf4687ba4778324ae6d
David Neu <mail@david-neu.de>
2019-03-12 11:12:29 +0100

    Logging Hain dev Path in local install

--- install-dev.js
+++ install-dev.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 
 var dir = process.env.LOCALAPPDATA+'/hain-user/devplugins/hain-plugin-tel/';
+console.log(dir);
 fs.copyFileSync("index.js", dir+"index.js");
 fs.copyFileSync("package.json", dir+"package.json");
